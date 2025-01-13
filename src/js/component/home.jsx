714f149607e7b17683f1a1ba4a35318a4e633352
Alejandro <alejandronicolasdelalamo@gmail.com>
2025-01-13 09:30:04 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,54 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [listElement, setListElement] = useState([]);
+
+  const handleNewTask = (e) => {
+    if (e.key === "Enter" && e.target.value !== "") {
+      setListElement([...listElement, e.target.value]);
+      e.target.value = "";
+    }
+  };
+  const handleClick = (e) => {
+    const newLi = listElement.filter((element) => {
+      return e.target.parentElement.firstChild.textContent !== element;
+    });
+    setListElement(newLi);
+  };
+  return (
+    <div className="container d-flex flex-column m-2 w-50 align-items-center p-5">
+      <h1>Lista de Tareas</h1>
+      <div className="input-group w-50">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Añadir tarea pendiente"
+          aria-label="Añadir tarea pendiente"
+          aria-describedby="basic-addon1"
+          onKeyDown={handleNewTask}
+        />
+      </div>
+      {listElement.map((element) => {
+        return (
+          <ul className="list-group w-50 d-flex justify-content-between">
+            <li className="list-group-item d-flex justify-content-between ">
+              {element}
+              <span className="button-delete" onClick={handleClick}>
+                x
+              </span>
+            </li>
+          </ul>
+        );
+      })}
+      <div className="list-group w-50">
+        <span className="list-group-item counter">
+          {listElement.length} Tareas pendientes{" "}
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
